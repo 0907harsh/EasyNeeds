89b5833dd3c39007e6c971e5ce431e1c1d9d5df0
Harsh Gupta <2000harshgupta@gmail.com>
2020-06-04 11:56:02 +0530

    Cross-site script attacks banned

--- public/js/app2.js
+++ public/js/app2.js
@@ -68,6 +68,7 @@ socket.on('isLoggedIn',(isLoggedIn)=>{
 })
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
+    // console.log('SEcond')
     const location =search.value
     loationpara.textContent='Loading...'
     currentpara.textContent=''
--- public/js/sanitize.js
+++ public/js/sanitize.js
@@ -1,8 +1,9 @@
 function sanitizeInput(element){
     // console.log(element.value)
     var str=element.value;
-    str=str.replace(/[<>!~`$()=;]/gim,"");
-    str=str.replace(/\/[a-z ]+|\/[A-Z]+|\//gim,"");
+    str=str.replace(/(<([^>]+)>)/ig,"");
+    // str=str.replace(/\/[a-z ]+|\/[A-Z]+|\/[^1-9]+/gim,"");
+    str=str.replace(/[(=);]/ig,"");
     str=str.trim();
     element.value=str;
 }
