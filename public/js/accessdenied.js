4990efc51aac077fd5c3a6c3c58500f709811050
Harsh Gupta <2000harshgupta@gmail.com>
2020-06-23 22:22:25 +0530

    Increasing Speed And Performance

--- public/js/accessdenied.js
+++ public/js/accessdenied.js
@@ -1,45 +1,3 @@
-// fetch('http://localhost:3000/weathr?address=!').then((response)=>{
-//   response.json().then((data)=>{
-//       if(data.error){
-//           console.log(data.error)
-//       }
-//       else{
-//           console.log(data.location)
-//           console.log(data.forecast)
-//       }
-//   })
-// })
-// const socket=io()
-const loationpara=document.querySelector('#locationpara')
-document.querySelector('#SubmitDetails').addEventListener('click',async (e)=>{
-    e.preventDefault()
-    const email=document.querySelector('#EmailUser').value
-    const password=document.querySelector('#Password').value
-    // socket.emit('DetailSubmit',username,password)
-    var data={email,password}
-    // console.log(data)
-    const response=await fetch('/login',{
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        body: JSON.stringify(data)
-    })
-    
-    const final = response.json()
-    // console.log(response.body)
-    if(response.status==202){
-        loationpara.innerHTML="<div class=\"uk-alert-success\" uk-alert><a class=\"uk-alert-close\" uk-close></a><p>Success.New Account Created</p></div>"  
-        location.replace('/')
-        // console.log(response.status)
-    }else{
-        loationpara.innerHTML="<div class=\"uk-alert-danger\" uk-alert><a class=\"uk-alert-close\" uk-close></a><p>Invalid Credentials. Please Try Again</p></div>"  
-    }
-    // console.log()
-})
-// console.log('HI EVERYONE')
-
 //get Cookie function
 var getCookie=async function getCookie(cname) {
     var name = cname + "=";
@@ -103,5 +61,3 @@ var LoginButton=async function LoginButton() {
   
   LoginButton()
   
-
-
--- public/js/app2.js
+++ public/js/app2.js
@@ -110,5 +110,82 @@ function gettingCookie(cname) {
     return "";
   }
 
+//Hiding Login/SignUp buttons
+var LoginButton=async function LoginButton() {
+    var isLoggedIn =await (await fetch('/loginstatus',{
+       method:'POST'
+    })).json()
+    // console.log(isLoggedIn)
+    if (!isLoggedIn) {
+      // console.log('Why the hell')
+      var all = document.getElementsByClassName('LoginPageButton');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.display = '';
+      }
+      all = document.getElementsByClassName('SignUpPageButton');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.display = '';
+      }
+      all = document.getElementsByClassName('MyProfilePageButton');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.display = 'none';
+      }
+    }
+    if (isLoggedIn) {
+      var all = document.getElementsByClassName('LoginPageButton');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.display = 'none';
+      }
+      all = document.getElementsByClassName('SignUpPageButton');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.display = 'none';
+      }
+      all = document.getElementsByClassName('MyProfilePageButton');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.display = '';
+      }
+    }
+  }
+  
+  LoginButton()
+
+
+
+
+  //santizing input
+  function sanitizeInput(element){
+    // console.log(element.value)
+    var str=element.value;
+    str=str.replace(/(<([^>]+)>)/ig,"");
+    // str=str.replace(/\/[a-z ]+|\/[A-Z]+|\/[^1-9]+/gim,"");
+    str=str.replace(/[(=);]/ig,"");
+    str=str.trim();
+    element.value=str;
+}
+//get Cookie Function
+var getCookie=async function getCookie(cname) {
+    var name = cname + "=";
+    
+    const response =await fetch('/serveCookie',{
+        method:'POST'
+    })
+    const res=await response.json()
+    var decodedCookies=JSON.stringify(res.userData)
+    decodedCookies="userData=j:"+decodedCookies
+    var ca = decodedCookies.split(';');
+    decodedCookies=""
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
 
 
