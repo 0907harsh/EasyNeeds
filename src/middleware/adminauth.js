9104504d1a4a62fa63b1b08e55df67a110434118
Harsh Gupta <2000harshgupta@gmail.com>
2020-07-01 19:37:47 +0530

    Bug FIXES

--- src/middleware/adminauth.js
+++ src/middleware/adminauth.js
@@ -2,7 +2,7 @@ const adminauth = async(req,res,next)=>{
     try{
         // console.log(req.headers.referer)
         // console.log(req.headers.host)
-        if(req.headers.referer===('http://'+req.headers.host+'/') || req.headers.referer===('https://'+req.headers.host))
+        if(req.headers.referer===('http://'+req.headers.host+'/') || req.headers.referer===('https://'+req.headers.host+'/'))
         next()
         else
         res.writeHead(302, { "Location": "https://" + req.headers['host'] + "/Accessdenied"}).end();
